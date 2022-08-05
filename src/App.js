3454fae631f95f5af9f4fa4492272388341b551f
Ramandeep <ramank6238299@gmail.com>
2022-08-05 12:49:52 +0530

    comment

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import { Header } from "./Components/Header.Component/header";
 import { LeftAside } from "./Components/LeftAside.Component/LeftAside";
 import { NewPost } from "./Components/NewPost.Component/NewPost";
+import { Comment } from "./Components/Comment.Component/Comment";
+
 function App() {
   return (
     <>
       <div className="App">
         {/* <Header />
         <LeftAside /> */}
-        <NewPost />
+        {/* <NewPost /> */}
+        <Comment />
       </div>
     </>
   );
